49d6d0ac1b18d21dff7428aca791c351dd68eeef
Maneet Goyal <maneetgoyal14@gmail.com>
2022-10-21 22:56:13 +0530

    Add all tests

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ Specifically, please consider doing the following, in no particular order:
  7) If counter goes below zero, make timer decrement rather than increment it. ✅
  8) Add a drop-down to select increment and decrement step between 1, 2, and 3. ✅
  9) Use monospace font for all text on the page. ✅
- 10) Create unit tests for increment, decrement and counter functions.
+ 10) Create unit tests for increment, decrement and counter functions. ✅
  11) Create package configuration that could be used to serve the page. ✅
  12) Change App to log the timestamp of when that component did mount. ✅
 */
--- src/unit.test.ts
+++ src/unit.test.ts
@@ -1,4 +1,4 @@
-import { increment, decrement, ActionTitles} from "./index";
+import { increment, decrement, ActionTitles, counter } from "./index";
 
 it('tests increment action creator', () => {
     expect(increment(1)).toEqual({type: ActionTitles.Increment, payload: 1});
@@ -10,4 +10,11 @@ it('tests decrement action creator', () => {
   expect(decrement(1)).toEqual({type: ActionTitles.Decrement, payload: 1});
   expect(decrement(2)).toEqual({type: ActionTitles.Decrement, payload: 2});
   expect(decrement(3)).toEqual({type: ActionTitles.Decrement, payload: 3});
+});
+
+it('tests counter reduces', () => {
+  expect(counter({count: 0, step: 1}, increment(1))).toEqual({count: 1, step: 1})
+  expect(counter({count: 0, step: 1}, increment(2))).toEqual({count: 2, step: 1})
+  expect(counter({count: 0, step: 1}, decrement(1))).toEqual({count: -1, step: 1})
+  expect(counter({count: 0, step: 1}, decrement(2))).toEqual({count: -2, step: 1})
 });
